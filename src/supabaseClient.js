2216caa0f61cb1b05d5854b5fdc794cf22bef8f0
MatheusLima505 <mathlegeferi@gmail.com>
2024-12-08 01:51:34 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wrofosfnenktzukayekp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indyb2Zvc2ZuZW5rdHp1a2F5ZWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5MjI0MDQsImV4cCI6MjA0ODQ5ODQwNH0.RMjHg-dPNcnv81zcajlAyG-ttbpu-ADm7xiNATKFm04';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
